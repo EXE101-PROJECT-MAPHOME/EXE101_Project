3234ba6c7ec83f99c428319db556e7295b19af3a
Phuong20042910 <phuongpdtse182721@fpt.edu.vn>
2026-06-09 11:09:15 +0700

    update config and add intro screen

--- MapHome_Frontend_Mobile_App_Expo/app/_layout.tsx
+++ MapHome_Frontend_Mobile_App_Expo/app/_layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout() {
               value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
             >
               <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
                 <Stack.Screen name="(auth)" options={{ headerShown: false }} />
                 <Stack.Screen
                   name="(tabs)"
